59466128fed14961dc0bfc836270088750e3d422
Emerlyy <vadim200339@gmail.com>
2022-11-05 23:24:18 +0200

    added bar chart

--- app/src/main/java/com/example/tik/ThirdActivity.java
+++ app/src/main/java/com/example/tik/ThirdActivity.java
@@ -6,12 +6,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThirdActivity extends AppCompatActivity {
 
+    BarChart barChart;
+    List<BarEntry> entries;
+    BarDataSet dataSet;
+    BarData barData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        barChart = (BarChart) findViewById(R.id.chart);
+        entries = new ArrayList<BarEntry>();
+        entries.add(new BarEntry(1,2));
+        entries.add(new BarEntry(2,1));
+        entries.add(new BarEntry(3,2));
+        entries.add(new BarEntry(4,7));
+        entries.add(new BarEntry(5,4));
+        dataSet = new BarDataSet(entries,"Label");
+        barData = new BarData(dataSet);
+        barChart.setData(barData);
+        barChart.invalidate();
     }
     public void goBack2(View v){
         Intent intent = new Intent(this,SecondActivity.class);
