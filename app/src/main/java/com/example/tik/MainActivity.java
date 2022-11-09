dc0840a7d01117374efc0a6455817e3be8cf6c9c
Emerlyy <vadim200339@gmail.com>
2022-11-09 13:36:50 +0200

    3

--- app/src/main/java/com/example/tik/MainActivity.java
+++ app/src/main/java/com/example/tik/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.transition.AutoTransition;
 import android.transition.TransitionManager;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.view.View;
@@ -14,7 +15,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView detailsText1,  detailsText2, detailsText3;
+    TextView detailsText1, detailsText2, detailsText3;
+    EditText editPeriod, editWidth, editAmplitude;
     RelativeLayout mainLayout;
 
     @Override
@@ -25,26 +27,47 @@ public class MainActivity extends AppCompatActivity {
         detailsText1 = findViewById(R.id.details1);
         detailsText2 = findViewById(R.id.details2);
         detailsText3 = findViewById(R.id.details3);
+        editPeriod = findViewById(R.id.editPeriod);
+        editWidth = findViewById(R.id.editWidth);
+        editAmplitude = findViewById(R.id.editAmplitude);
         mainLayout = findViewById(R.id.mainLayout);
         //layout.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
         mainLayout.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
 
 
     }
-    public void startNewActivity(View v){
-        Intent intent = new Intent(this,SecondActivity.class);
+
+    public void startNewActivity(View v) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        int period = tryParse(editPeriod.getText().toString());
+        int width = tryParse(editWidth.getText().toString());
+        int amplitude = tryParse(editAmplitude.getText().toString());
+        intent.putExtra("period", period);
+        intent.putExtra("width",width);
+        intent.putExtra("amplitude",amplitude);
         startActivity(intent);
     }
-    public void expand1(View view){
-        int v = (detailsText1.getVisibility()==view.GONE)?View.VISIBLE:View.GONE;
+
+    public void expand1(View view) {
+        int v = (detailsText1.getVisibility() == view.GONE) ? View.VISIBLE : View.GONE;
         detailsText1.setVisibility(v);
     }
-    public void expand2(View view){
-        int v = (detailsText2.getVisibility()==view.GONE)?View.VISIBLE:View.GONE;
+
+    public void expand2(View view) {
+        int v = (detailsText2.getVisibility() == view.GONE) ? View.VISIBLE : View.GONE;
         detailsText2.setVisibility(v);
     }
-    public void expand3(View view){
-        int v = (detailsText3.getVisibility()==view.GONE)?View.VISIBLE:View.GONE;
+
+    public void expand3(View view) {
+        int v = (detailsText3.getVisibility() == view.GONE) ? View.VISIBLE : View.GONE;
         detailsText3.setVisibility(v);
+    }
+
+    public static Integer tryParse(String text) {
+        try {
+            return Integer.parseInt(text);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
     }
 }
--- app/src/main/java/com/example/tik/SecondActivity.java
+++ app/src/main/java/com/example/tik/SecondActivity.java
@@ -1,17 +1,28 @@
 package com.example.tik;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 
 public class SecondActivity extends AppCompatActivity {
-
+    int period,width,amplitude;
+    CheckBox checkBox1,checkBox2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        period = getIntent().getIntExtra("period", 0);
+        width = getIntent().getIntExtra("width", 0);
+        amplitude = getIntent().getIntExtra("amplitude", 0);
+        checkBox1 = findViewById(R.id.checkBox);
+        checkBox1.setText(Double.toString(calcA0(period, width, amplitude)));
+        checkBox2 = findViewById(R.id.checkBox2);
+        checkBox2.setText(Double.toString(calcAn(period, width, amplitude, 1)));
     }
     public void startNewActivity2(View v){
         Intent intent = new Intent(this,ThirdActivity.class);
@@ -21,4 +32,14 @@ public class SecondActivity extends AppCompatActivity {
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
     }
+    public double calcA0(int T,int t,int h){
+        return (double)h*t/T;
+    }
+
+    public double calcAn(int T,int t,int h,int k) {
+        double w = 2 * Math.PI / T;
+        double result = 2 * h * t * Math.sin(k * w * t / 2) / (T * k * w * t / 2);
+        return Double.parseDouble(String.format("%.2f", result));
+    }
+
 }
