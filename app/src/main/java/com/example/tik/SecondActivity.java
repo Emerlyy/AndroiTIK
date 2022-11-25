6ca601c83956178a6433795be36c5a8c6e2d1bf2
Bohdan <bohdan.baluh@gmail.com>
2022-11-25 20:26:43 +0200

    6.2.1

--- app/src/main/java/com/example/tik/SecondActivity.java
+++ app/src/main/java/com/example/tik/SecondActivity.java
@@ -5,14 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.SpannableString;
 import android.text.Spanned;
+import android.text.TextWatcher;
 import android.text.style.RelativeSizeSpan;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ToggleButton;
-
+import android.widget.Button;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -23,7 +25,7 @@ public class SecondActivity extends AppCompatActivity {
     CheckBox[] arrayCheckBox = new CheckBox[16];
     float[] arrayData = new float[arrayCheckBox.length];
     ToggleButton checkAllButton;
-
+    Button nextStepButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +37,7 @@ public class SecondActivity extends AppCompatActivity {
         int amplitude = getIntent().getIntExtra("amplitude", 0);
 
         arrayCheckBox[0] = (CheckBox) findViewById(R.id.checkBox0);
+        nextStepButton = (Button) findViewById(R.id.next);
         arrayData[0] = calcA0(period, width, amplitude);
         SpannableString text = new SpannableString("A0 = " + arrayData[0]);
         text.setSpan(new RelativeSizeSpan(0.6f), 1, 2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
@@ -48,17 +51,14 @@ public class SecondActivity extends AppCompatActivity {
             text.setSpan(new RelativeSizeSpan(0.6f), 1, String.valueOf(i).length() + 2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
             arrayCheckBox[i].setText(text);
 
-
         }
         for (int i = 0; i < arrayCheckBox.length; i++) {
             arrayCheckBox[i].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
                 public void onCheckedChanged(CompoundButton buttonView, boolean isChacked) {
-
                     updateButton();
                 }
             });
-
         }
         checkAllButton = findViewById(R.id.checkAllButton);
         boolean isChecked = checkAllButton.isChecked();
@@ -69,6 +69,23 @@ public class SecondActivity extends AppCompatActivity {
                 updateVisibility(checkAllButton.isChecked());
             }
         });
+        nextStepButton.setEnabled(true);
+        for (int i = 0; i < arrayCheckBox.length; i++) {
+            arrayCheckBox[i].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                    if(arrayCheckBox[0].isChecked()||arrayCheckBox[1].isChecked()||arrayCheckBox[2].isChecked()||arrayCheckBox[3].isChecked()||arrayCheckBox[4].isChecked()||
+                            arrayCheckBox[5].isChecked()||arrayCheckBox[6].isChecked()||arrayCheckBox[7].isChecked()||arrayCheckBox[8].isChecked()|| arrayCheckBox[9].isChecked()||
+                            arrayCheckBox[10].isChecked()||arrayCheckBox[11].isChecked()||arrayCheckBox[12].isChecked()||arrayCheckBox[13].isChecked()||arrayCheckBox[14].isChecked()||arrayCheckBox[15].isChecked()) {
+                        nextStepButton.setEnabled(true);
+                    }
+                    else{
+                        nextStepButton.setEnabled(false);
+                    }
+                }
+            });
+        }
 
     }
 
@@ -84,7 +101,6 @@ public class SecondActivity extends AppCompatActivity {
 
     private void updateVisibility(boolean isChecked) {
         if (isChecked) {
-
             for (int i = 0; i < arrayCheckBox.length; i++) {
                 arrayCheckBox[i].setChecked(true);
             }
@@ -96,15 +112,14 @@ public class SecondActivity extends AppCompatActivity {
         }
     }
 
-
     public void startNewActivity2(View v) {
         List<Float> absData = new ArrayList<>();
         List<Integer> indexes = new ArrayList<>();
         Intent intent = new Intent(this, ThirdActivity.class);
         for (int i = 0; i < arrayData.length; i++) {
             if (arrayCheckBox[i].isChecked()) {
-                absData.add(Math.abs(arrayData[i]));
-                indexes.add(i);
+                    absData.add(Math.abs(arrayData[i]));
+                        indexes.add(i);
             }
         }
         intent.putExtra("data", (Serializable) absData);
